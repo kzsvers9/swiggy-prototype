d985633a2a303932756538099b1de3261802bf27
Kshitiz Sonakiya <kzsvers9@gmail.com>
2023-03-27 12:57:36 +0530

    redid changes

--- src/MainList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MainList({classname, listtext}) {
-  return (
-    <a href="" className="ctg"><div className={classname}>{listtext}</div></a>
-  )
-}
-
-export default MainList
--- src/MainNavbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MainNavbar({ className, navtext }) {
-  return (
-        <span className={className}>{navtext}</span>
-  )
-}
-
-export default MainNavbar
